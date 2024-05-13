358400fd9cc01cb201bbd404712d2e388d35ce11
Kiran Katti <139916130+kirankattii@users.noreply.github.com>
2024-05-13 07:56:26 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,4 +15,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//Routes import
+import userRouter from "./routes/user.routes.js";
+
+//router decleration
+app.use("/api/v1/users", userRouter);
+
 export { app };
